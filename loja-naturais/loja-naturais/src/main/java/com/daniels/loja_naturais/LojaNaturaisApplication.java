38061a03f348baf128bbd83ca13444c593e7701c
Daniel Santos <166452209+star-Daniels@users.noreply.github.com>
2025-06-25 10:44:41 -0300

    Add files via upload

---
+++ loja-naturais/loja-naturais/src/main/java/com/daniels/loja_naturais/LojaNaturaisApplication.java
@@ -0,0 +1,13 @@
+package com.daniels.loja_naturais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaNaturaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaNaturaisApplication.class, args);
+	}
+
+}
---
+++ loja-naturais/loja-naturais/src/test/java/com/daniels/loja_naturais/LojaNaturaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniels.loja_naturais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaNaturaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
